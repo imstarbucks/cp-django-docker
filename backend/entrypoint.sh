2a6b7df4c294524589000795c36d7376d7925585
imstarbucks <spenwong98@gmail.com>
2023-04-04 22:52:23 +0800

    Update: Fixed serving static files

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,7 +7,8 @@ python3 manage.py collectstatic --no-input
 # Start server
 echo "Starting server"
 
-# python3 manage.py runserver 0.0.0.0:8000
 gunicorn server.wsgi:application --bind 0.0.0.0:8000
+python manage.py runserver
+# python3 manage.py runserver 0.0.0.0:8000
 
 exec "$@"
