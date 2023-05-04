562967c71128a0f3cc3ef50d68405fa560e390ea
imstarbucks <spenwong98@gmail.com>
2023-05-04 13:27:30 +0800

    Update: Postgres entrtypoint check

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# Check Postgres is healthy
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
 # Collect static files
 echo "Collect static files"
 python3 manage.py collectstatic --no-input
